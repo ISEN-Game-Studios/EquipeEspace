a277ad0449c7163ef3e809724b3f1cc24d84ee02
Zalpheges <geoffreey.bernard@gmail.com>
2022-02-19 16:22:02 +0100

    Server Base

--- Serverside Code/Game Code/Game.cs
+++ Serverside Code/Game Code/Game.cs
@@ -5,18 +5,26 @@ using System;
 public class Player : BasePlayer
 {
 	public bool ready = false;
+	public bool boarded = false;
+
+	public int[] usedIDs;
 }
 
 [RoomType("SpaceShip")]
 public class GameCode : Game<Player>
 {
+	private int current;
+
+	private new List<Player> Players;
+
+	private List<int> usedIDs;
+
+	private bool isReady = false;
+	private bool isRunning = false;
+
 	public override void GameStarted()
 	{
-        //RoomId
-        //AddTimer
-        //Players
-        //PlayerCount
-        //Broadcast
+		Players = new List<Player>();
 	}
 
 	public override void GameClosed()
@@ -26,25 +34,52 @@ public class GameCode : Game<Player>
 
 	public override void UserJoined(Player player)
 	{
+		if (isReady)
+			player.Disconnect();
+		else
+		{
+			Players.Add(player);
 
+			if (Players.Count == 1)
+				player.ready = true;
+		}
 	}
 
 	public override void UserLeft(Player player)
 	{
-
+		if (isReady && Players.Contains(player))
+		{
+			foreach (Player _player in Players)
+				_player.Disconnect();
+		}
 	}
 
 	public override void GotMessage(Player player, Message message)
 	{
 		switch (message.Type)
 		{
-			case "Play":
+			case "Ready":
+				player.ready = message.GetBoolean(0);
 
+				if (Players.TrueForAll(waiter => waiter.ready))
+                {
+					isReady = true;
+
+					GenerateBoards();
+				}
 
 				break;
 
-			case "Ready":
-				player.ready = true;
+			case "Boarded":
+				int[] ids = (int[])message[0];
+
+				player.usedIDs = ids;
+				usedIDs.AddRange(ids);
+
+				if (++current < Players.Count)
+					Players[current].Send("Board", usedIDs.ToArray());
+				else
+					isRunning = true;
 
 				break;
 
@@ -57,6 +92,9 @@ public class GameCode : Game<Player>
 
 	private void GenerateBoards()
     {
+		current = 0;
+		usedIDs.Clear();
 
+		Players[0].Send("Board", usedIDs);
     }
 }
