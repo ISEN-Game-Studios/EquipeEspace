7e19feda0bb05db86f08e90f197485b897879a1d
CriLénine <remi.dufosse@student.yncrea.fr>
2022-02-24 23:00:40 +0100

    input anchorFix

--- Assets/Scripts/Button/Interactable.cs
+++ Assets/Scripts/Button/Interactable.cs
@@ -16,6 +16,11 @@ public abstract class Interactable : MonoBehaviour
 
     [SerializeField] protected List<TextMesh> textMeshsButtons;
 
+    protected bool _broken = true;
+    protected bool _dragging = false;
+    protected TargetJoint2D _targetJoint;
+    protected Rigidbody2D _rigidbody;
+
     protected virtual void Start()
     {
         textMeshWrapper.SetText(itemData.name);
@@ -30,6 +35,63 @@ public abstract class Interactable : MonoBehaviour
                 textMeshsButtons[index].text = itemData.Values[index];
             }
         }
+
+        _targetJoint = GetComponent<TargetJoint2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _broken = false;
+        _dragging = false;
+    }
+
+    private void Update()
+    {
+        //if(Input.touchCount != 1)
+        //{
+        //    dragging = false;
+        //    GetComponentInChildren<TargetJoint2D>().enabled = false;
+        //    return;
+        //}
+
+        if (Input.touchCount > 1)
+        {
+            _dragging = false;
+            _targetJoint.enabled = false;
+            return;
+        }
+
+        //Touch touch = Input.GetTouch(0);
+        //Vector3 pos = touch.position;
+        Vector2 posM = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (/*touch.phase == TouchPhase.Began ||*/ Input.GetMouseButtonDown(0))
+        {
+            if (Physics2D.Raycast(posM, Vector3.forward))
+            {
+                if (_broken)
+                    _targetJoint.enabled = true;
+            }
+        }
+        if ((/*touch.phase == TouchPhase.Moved ||*/ Input.GetMouseButton(0)) && _broken)
+        {
+            _dragging = true;
+            _targetJoint.target = posM;
+        }
+        else if (_dragging && Input.GetMouseButtonUp(0)/*(touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)*/)
+        {
+            _dragging = false;
+            _targetJoint.enabled = false;
+            if (Mathf.Abs(_rigidbody.rotation) <= 5)
+            {
+                _broken = false;
+                _rigidbody.isKinematic = true;
+                _rigidbody.angularVelocity = 0f;
+                transform.rotation = Quaternion.identity;
+            }
+        }
+    }
+
+    public void Break()
+    {
+        _rigidbody.isKinematic = false;
     }
 
     protected void SendState(int state)
