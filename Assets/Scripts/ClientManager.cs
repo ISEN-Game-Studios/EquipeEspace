3df3aa83a20033c93b246a87b489becf0d30b786
Zalpheges <geoffreey.bernard@gmail.com>
2022-03-05 22:24:37 +0100

    Server OK

--- Assets/Scripts/ClientManager.cs
+++ Assets/Scripts/ClientManager.cs
@@ -67,7 +67,7 @@ public class ClientManager : MonoBehaviour
 			delegate (Client client) {
 				Debug.Log("Successfully connected to Player.IO");
 
-				client.Multiplayer.DevelopmentServer = new ServerEndpoint("localhost", 8184);
+				//client.Multiplayer.DevelopmentServer = new ServerEndpoint("localhost", 8184);
 
 				client.Multiplayer.CreateJoinRoom(
 					roomname,
